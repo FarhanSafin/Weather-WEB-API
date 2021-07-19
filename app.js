9da2b260a18803a5ba51bec8c26612b745c934d3
FarhanSafin <52666674+FarhanSafin@users.noreply.github.com>
2021-07-19 12:21:20 +0600

    v2.0 FIN
    Weather API-First-NODE-EXPRESS
    OPENWEATHER.ORG

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const { response } = require("express");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req,res){
-    res.send("HELLO ADMIN");
-    const url="https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=737f67de0f2cb801f61437647cddf9e3&units=metric";
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+app.post("/", function(req,res){
+    const Location = req.body.cityName;
+    const apiKey = "737f67de0f2cb801f61437647cddf9e3";
+    const units = "metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+ Location +"&appid="+ apiKey +"&units=" + units;
 
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -16,11 +25,18 @@ app.get("/", function(req,res){
             const weatherData = JSON.parse(data);
             const weatherFeels = weatherData.main.feels_like;
             const weatherDescrip = weatherData.weather[0].description;
-            console.log(weatherFeels);
-            console.log(weatherDescrip);
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>The temp in " + Location + " is: "+ weatherFeels+ " degree celcius</h1>");
+            res.write("<p>The description of the weather is: " + weatherDescrip + "</p>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
         })
     })
 })
+
+
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
